5046b2a89df0ecf6aa37d8fe1d62eb9d40c2ba36
drojas87-photo <diegorojas87@protonmail.com>
2024-06-11 20:27:12 -0300

    agregada primera version traduccion

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
-const flagsElement = document.getElementById("flags")
+const flagsElement = document.getElementById("flags");
 
 
 const changeLanguage = async language => {
-    const requestJson = await fetch (`./translationes/${language}.json`)
+    
+    const requestJson = await fetch (`translations/${language}.json`)
     const texts = await requestJson.json()
 }
 
 flagsElement.addEventListener("click" , (e) => {
+    console.log(e.target.parentElement.dataset.language);
     changeLanguage(e.target.parentElement.dataset.language)
 });
