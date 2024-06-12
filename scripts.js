04b339914faceea4f5e45dbb38006acf019efb39
drojas87-photo <diegorojas87@protonmail.com>
2024-06-11 21:27:47 -0300

    agregada transicion paginas, primera version acerca de , agregado logo a todas las paginas

--- scripts.js
+++ scripts.js
@@ -15,7 +15,23 @@ const changeLanguage = async language => {
     }
 }
 
+
+const images = document.querySelectorAll('.hero-image');
+let currentIndex = 0;
+
+images[currentIndex].classList.add('active');
+
+    function showNextImage() {
+        images[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add('active');
+    }
+
+    setInterval(showNextImage, 10000); // 
+
 flagsElement.addEventListener("click" , (e) => {
     console.log(e.target.parentElement.dataset.language);
     changeLanguage(e.target.parentElement.dataset.language)
 });
+
+
